db669a27a3c1969ebdf0c90e15c797de5abce2c9
patriziamarciniak <patriziamarciniak@wp.pl>
2016-04-15 01:06:11 +0200

    First commit

---
+++ app/src/main/java/com/example/hp/firstapp2/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.hp.firstapp2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        Button btnMyHistory = (Button) findViewById(R.id.button3);
+        btnMyHistory.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                Intent nextScreen = new Intent(getApplicationContext(), MyHistory.class);
+                startActivity(nextScreen);
+            }
+        });
+
+        Button btnMyEvents = (Button) findViewById(R.id.button2);
+        btnMyEvents.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                Intent nextScreen = new Intent(getApplicationContext(), ComingTrips.class);
+                startActivity(nextScreen);
+            }
+        });
+
+        Button btnAddEvent = (Button) findViewById(R.id.button1);
+        btnAddEvent.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                Intent nextScreen = new Intent(getApplicationContext(), AddEvent.class);
+                startActivity(nextScreen);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
