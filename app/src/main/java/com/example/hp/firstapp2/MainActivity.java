b9669837d9aeadf017ed7befd3085c1217bdc5ca
patriziamarciniak <patriziamarciniak@wp.pl>
2016-04-26 14:21:07 +0200

    Added language change option

--- app/src/main/java/com/example/hp/firstapp2/MainActivity.java
+++ app/src/main/java/com/example/hp/firstapp2/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.hp.firstapp2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,8 +11,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
+    Context context = this;
+    Button btnMyHistory, btnMyEvents, btnAddEvent ;
+    Locale locale;
+    Configuration config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +27,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        locale = new Locale("pl");
+        Locale.setDefault(locale);
+        config = new Configuration();
+        config.locale = locale;
+        context.getApplicationContext().getResources().updateConfiguration(config, null);
 
-        Button btnMyHistory = (Button) findViewById(R.id.button3);
+
+        btnMyHistory = (Button) findViewById(R.id.button3);
         btnMyHistory.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View arg0) {
@@ -31,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btnMyEvents = (Button) findViewById(R.id.button2);
+        btnMyEvents = (Button) findViewById(R.id.button2);
         btnMyEvents.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View arg0) {
@@ -40,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btnAddEvent = (Button) findViewById(R.id.button1);
+        btnAddEvent = (Button) findViewById(R.id.button1);
         btnAddEvent.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View arg0) {
@@ -48,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(nextScreen);
             }
         });
+        changeItemTranslation();
 
     }
 
@@ -60,16 +73,38 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                return true;
+            case R.id.action_changeLanguage:
+                return true;
+            case R.id.lang_pl:
+                locale = new Locale("pl");
+                Locale.setDefault(locale);
+                config = new Configuration();
+                config.locale = locale;
+                context.getApplicationContext().getResources().updateConfiguration(config, null);
+                changeItemTranslation();
+                return true;
+            case R.id.lang_en:
+                locale = new Locale("en");
+                Locale.setDefault(locale);
+                config = new Configuration();
+                config.locale = locale;
+                context.getApplicationContext().getResources().updateConfiguration(config, null);
+                changeItemTranslation();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    private void changeItemTranslation(){
+
+        btnAddEvent.setText(R.string.title_activity_add_event);
+        btnMyEvents.setText(R.string.title_coming_trips);
+        btnMyHistory.setText(R.string.title_history);
     }
 }
